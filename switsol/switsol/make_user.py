b844fc25ec5640d22f115297c5f2bf49ed3574a9
khatrijitendra <jitendra.k@indictranstech.com>
2016-10-21 13:00:58 +0530

    [Fix]changes in make_user logged in and call logs report

--- switsol/switsol/make_user.py
+++ switsol/switsol/make_user.py
@@ -3,7 +3,6 @@ from frappe import _
 from frappe.utils.password import check_password
 from datetime import datetime,date
 from frappe.model.mapper import get_mapped_doc
-#from switsol.switsol.boot import boot_session
 
 @frappe.whitelist(allow_guest=True)
 def logged_and_redirect(user_name,password,email,contact_no):
@@ -30,7 +29,7 @@ def logged_and_redirect(user_name,password,email,contact_no):
 		user.flags.ignore_permissions = True
 		user.flags.no_welcome_mail = True
 		user.save()
-		user.add_roles("Accounts User")
+		user.add_roles("Accounts User","Purchase Master Manager","Sales Master Manager")
 		make_redirect_url(email,contact_no)
 
 	if frappe.session.user and frappe.session.user == email:
@@ -40,11 +39,9 @@ def logged_and_redirect(user_name,password,email,contact_no):
 	elif email == check_password(email,password):
 		make_redirect_url(email,contact_no)
 	 		
-	# if user_name == "administrator" or user_name == "Administrator":
-	# 	make_redirect_url(email,customer,contact_no)
-
 
 def make_redirect_url(email,contact_no,flag=None):
+	print contact_no,"contact_no","\n\n\n\n\n"
 	if frappe.db.get_value("Contact",{"mobile_no":contact_no},"name"):
 		contact_not_found = "No"
 		contact_person_list = frappe.db.sql("""select name,CASE WHEN customer IS NULL AND supplier IS NULL THEN concat("sales_partner","-",sales_partner) 
@@ -56,50 +53,54 @@ def make_redirect_url(email,contact_no,flag=None):
 			for e in contact_person_list:
 				contact_person_name_str += e["contact_person"]+","
 				if e["contact_person"].split("-")[0] == "supplier":
-					frappe.db.sql("""update `tabSupplier` set call_comming_from  = '{0}' where name =  '{1}'""".format(contact_no+"/"+e["name"],e["contact_person"].split("-")[1]))		
+					update_call_comming_from("Supplier",contact_no+"/"+e["name"],e["contact_person"].split("-")[1])
 				if e["contact_person"].split("-")[0] == "customer":
-					frappe.db.sql("""update `tabCustomer` set call_comming_from  = '{0}' where name =  '{1}'""".format(contact_no+"/"+e["name"],e["contact_person"].split("-")[1]))
+					update_call_comming_from("Customer",contact_no+"/"+e["name"],e["contact_person"].split("-")[1])
 				if e["contact_person"].split("-")[0] == "sales_partner":
-					frappe.db.sql("""update `tabSales Partner` set call_comming_from  = '{0}' where name =  '{1}'""".format(contact_no+"/"+e["name"],e["contact_person"].split("-")[1]))
-			frappe.db.sql("""update `tabSingles` set value = '{0}' 
-			where field = "contact_person_list" """.format(contact_person_name_str))			
+					update_call_comming_from("Sales Partner",contact_no+"/"+e["name"],e["contact_person"].split("-")[1])
+
+			update_tabsingles(contact_person_name_str,"contact_person_list")
 			url = "contact_search-"
 			set_redirect_url(email,url,contact_not_found,flag)
 
 		if len(contact_person_list) == 1:				
 			contact_person = frappe.db.get_value("Contact",{"mobile_no":contact_no},"name")
 			contact_doc = frappe.get_doc("Contact",frappe.db.get_value("Contact",{"mobile_no":contact_no},"name"))
-			frappe.db.sql("""update `tabSingles` set value = 'ABCD' 
-			where field = "contact_person_list" """)
+			update_tabsingles("ABCD","contact_person_list")
+
 			if contact_doc.customer:
-				url = "Form/Customer/{0}".format(contact_doc.customer)			
-				frappe.db.sql("""update `tabCustomer` set call_comming_from  = '{0}' where name =  '{1}'""".format(contact_no+"/"+contact_person,contact_doc.customer))
-				# customer_doc = frappe.get_doc("Customer",contact_doc.customer)
-				# customer_doc.call_comming_from = contact_no+"/"+contact_person
-				# customer_doc.save(ignore_permissions=True)
+				url = "Form/Customer/{0}".format(contact_doc.customer)
+				update_call_comming_from("Customer",contact_no+"/"+contact_person,contact_doc.customer)			
 				set_redirect_url(email,url,contact_not_found,flag)
+
 
 			if contact_doc.supplier:
 				url = "Form/Supplier/{0}".format(contact_doc.supplier)
-				frappe.db.sql("""update `tabSupplier` set call_comming_from  = '{0}' where name =  '{1}'""".format(contact_no+"/"+contact_person,contact_doc.supplier))
-				# supplier_doc = frappe.get_doc("Supplier",contact_doc.supplier)
-				# supplier_doc.call_comming_from = contact_no+"/"+contact_person
-				# supplier_doc.save(ignore_permissions=True)
+				update_call_comming_from("Customer",contact_no+"/"+contact_person,contact_doc.supplier)
 				set_redirect_url(email,url,contact_not_found,flag)
+
 				
 			if contact_doc.sales_partner:
 				url = "Form/Sales Partner/{0}".format(contact_doc.sales_partner)			
-				frappe.db.sql("""update `tabSales Partner` set call_comming_from  = '{0}' where name =  '{1}'""".format(contact_no+"/"+contact_person,contact_doc.sales_partner))
-				# sales_partner_doc = frappe.get_doc("Sales Partner",contact_doc.sales_partner)
-				# sales_partner_doc.call_comming_from = contact_no+"/"+contact_person
-				# sales_partner_doc.save(ignore_permissions=True)
+				update_call_comming_from("Sales Partner",contact_no+"/"+contact_person,contact_doc.sales_partner)
 				set_redirect_url(email,url,contact_not_found,flag)		
 
+
 	else:
+		update_tabsingles("ABCD","contact_person_list")
 		url = "contact_search-"
 		contact_not_found = "Yes"
 		set_redirect_url(email,url,contact_not_found,flag)
 
+def update_tabsingles(value,field):
+	frappe.db.sql("""update `tabSingles` set value = '{0}' 
+		 	where field = "{1}" """.format(value,field))
+	frappe.db.commit()
+
+def update_call_comming_from(table_name,call_comming_from,contact_reference):
+	frappe.db.sql("""update `tab{0}` set call_comming_from  = '{1}' \
+	 where name =  '{2}'""".format(table_name,call_comming_from,contact_reference))
+	frappe.db.commit()
 
 def set_redirect_url(email,url,contact_not_found,flag):
 	if flag:
@@ -116,9 +117,14 @@ def redirect_login(desk_user,url,contact_not_found):
 	# the #desktop is added to prevent a facebook redirect bug
 	frappe.local.response["location"] = "/desk#{0}".format(url)
 	if contact_not_found == "Yes":
-		frappe.db.sql("""update `tabSingles` set value = {0} 
-			where field = "voip_contact_no" """.format(frappe.local.form_dict["contact_no"]))
-		frappe.db.commit()
+		update_tabsingles(frappe.local.form_dict["contact_no"],"voip_contact_no")
+
+@frappe.whitelist()
+def get_contact_no_and_contact_person_list():
+	return frappe.db.sql("""select value,field from `tabSingles` 
+						where doctype = "VOIP Contact" """,as_dict=1)
+
+
 # old url
 
 #http://localhost:9090/api/method/switsol.switsol.make_user.logged_and_redirect?user_name=jitendra&email=jitendra.k@indictranstech.com&password=khatri&customer=Sangram&contact_no=4555
@@ -126,67 +132,4 @@ def redirect_login(desk_user,url,contact_not_found):
 # new url
 
 #http://5.148.186.248:9090/api/method/switsol.switsol.make_user.logged_and_redirect?user_name=&email=&password=&contact_no=
-#http://localhost:9090/api/method/switsol.switsol.make_user.logged_and_redirect?user_name=jitendra&email=jitendra.k@indictranstech.com&password=khatri&contact_no=4555
-
-
-
-
-@frappe.whitelist()
-def get_contact_no_and_contact_person_list():
-	return frappe.db.sql("""select value,field from `tabSingles` 
-						where doctype = "VOIP Contact" """,as_dict=1)
-
-@frappe.whitelist()
-def get_call_logs_from_customer(source_name, target_doc=None):
-	customer = frappe.get_doc("Customer",source_name)
-	target_doc = get_mapped_doc("Customer", source_name,
-		{
-			"Customer": {
-				"doctype": "Call Logs",
-			},
-		}, target_doc)
-
-	target_doc.client = str(customer.name)
-	target_doc.phone_number = str(customer.call_comming_from).split("/")[0]
-	target_doc.contact_person = str(customer.call_comming_from).split("/")[1]
-	target_doc.contact_type = "Customer"
-	target_doc.start_time = str(datetime.now()).split(".")[0]
-	target_doc.call_attendant = frappe.session.user
-
-	return target_doc
-
-@frappe.whitelist()
-def get_call_logs_from_supplier(source_name, target_doc=None):
-	supplier = frappe.get_doc("Supplier",source_name)
-	target_doc = get_mapped_doc("Supplier", source_name,
-		{
-			"Supplier": {
-				"doctype": "Call Logs",
-			},
-		}, target_doc)
-
-	target_doc.client = str(supplier.name)
-	target_doc.phone_number = str(supplier.call_comming_from).split("/")[0]
-	target_doc.contact_person = str(supplier.call_comming_from).split("/")[1]
-	target_doc.contact_type = "Supplier"	
-	target_doc.start_time = datetime.now()
-	target_doc.call_attendant = frappe.session.user	
-	return target_doc	
-
-@frappe.whitelist()
-def get_call_logs_from_sales_partner(source_name, target_doc=None):
-	sales_partner = frappe.get_doc("Sales Partner",source_name)
-	target_doc = get_mapped_doc("Sales Partner", source_name,
-		{
-			"Sales Partner": {
-				"doctype": "Call Logs",
-			},
-		}, target_doc)
-
-	target_doc.client = str(sales_partner.name)
-	target_doc.phone_number = str(sales_partner.call_comming_from).split("/")[0]
-	target_doc.contact_person = str(sales_partner.call_comming_from).split("/")[1]
-	target_doc.contact_type = "Sales Partner"
-	target_doc.start_time = datetime.now()
-	target_doc.call_attendant = frappe.session.user	
-	return target_doc
+#http://localhost:9091/api/method/switsol.switsol.make_user.logged_and_redirect?user_name=jitendra&email=jitendra.k@indictranstech.com&password=khatri&contact_no=4555	
--- switsol/switsol/report/call_logs_report/call_logs_report.py
+++ switsol/switsol/report/call_logs_report/call_logs_report.py
@@ -13,34 +13,34 @@ def execute(filters=None):
 def get_data(filters):
 	if filters:
 		print "\n\n\n\n\n\n","filters",filters
-		result = frappe.db.sql("""select date(creation),phone_number,contact_person,client, 
- 								time(start_time),time(end_time),TIMEDIFF(time(start_time),time(end_time)),call_attendant,
- 								name FROM `tabCall Logs` {0} order by name asc""" .format(get_cond(filters)),as_list=1,debug=1)
+		result = frappe.db.sql("""select name,date(creation),phone_number,contact_person,client,contact_type, 
+ 								time(start_time),time(end_time),TIMEDIFF(time(start_time),time(end_time)),call_attendant,subject
+ 								FROM `tabCall Logs` {0} order by name asc""" .format(get_cond(filters)),as_list=1,debug=1)
 
 	else:
-		result = frappe.db.sql("""select date(creation),phone_number,contact_person,client, 
- 								time(start_time),time(end_time),TIMEDIFF(time(start_time),time(end_time)),call_attendant,
- 								name FROM `tabCall Logs` order by name asc""",as_list=1)	
+		result = frappe.db.sql("""select name,date(creation),phone_number,contact_person,client,contact_type, 
+ 								time(start_time),time(end_time),TIMEDIFF(time(start_time),time(end_time)),call_attendant,subject
+ 								FROM `tabCall Logs` order by name asc""",as_list=1,debug=1)	
 	return result
 
 def get_cond(filters):
 	cond = ""
-	if filters.get('client_info').split("-")[0] == "Call Logs":
-		cond = "where phone_number = '{0}' and client = '{1}'".format(filters.get('client_info').split("-")[-1],filters.get('client_info').split("-")[2])
-	if filters.get('client_info').split("-")[0] == "Contact":
-		cond = "where phone_number = '{0}' and client = '{1}'".format(filters.get('client_info').split("-")[2],filters.get('client_info').split("-")[1])						
-	if filters.get('client_info').split("-")[0] == "Customer":
-		cond = "where client = '{0}' and contact_type = 'Customer' ".format(filters.get('client_info').split("-")[1])
-	if filters.get('client_info').split("-")[0] == "Supplier":
-		cond = "where client = '{0}' and contact_type = 'Supplier' ".format(filters.get('client_info').split("-")[1])
-	if filters.get('client_info').split("-")[0] == "Sales Partner":
-		cond = "where client = '{0}' and contact_type = 'Sales Partner' ".format(filters.get('client_info').split("-")[1])
+	if filters.get('client_info').split("//")[0] == "Call Logs":
+		cond = "where phone_number = '{0}' and client = '{1}'".format(filters.get('client_info').split("//")[-1],filters.get('client_info').split("//")[2])
+	if filters.get('client_info').split("//")[0] == "Contact":
+		cond = "where phone_number = '{0}' and contact_person = '{1}'".format(filters.get('client_info').split("//")[2],filters.get('client_info').split("//")[1])						
+	if filters.get('client_info').split("//")[0] == "Customer":
+		cond = "where client = '{0}' and contact_type = 'Customer' ".format(filters.get('client_info').split("//")[1])
+	if filters.get('client_info').split("//")[0] == "Supplier":
+		cond = "where client = '{0}' and contact_type = 'Supplier' ".format(filters.get('client_info').split("//")[1])
+	if filters.get('client_info').split("//")[0] == "Sales Partner":
+		cond = "where client = '{0}' and contact_type = 'Sales Partner' ".format(filters.get('client_info').split("//")[1])
 	return cond	
 
 def get_colums():
-	columns = [("Date") + ":Date:80"] + [("Phone Number") + ":Data:70"] + \
-			  [("Conact Person") + ":Data:150"] + \
-			  [("Client") + ":Data:100"] + [("Start Time") + ":Data:100"] + \
+	columns = [("Call Log Id") + ":Link/Call Logs:"] + [("Date") + ":Date:80"] + [("Phone Number") + ":Data:70"] + \
+			  [("Conact Person") + ":Link/Contact:150"] + \
+			  [("Client") + ":Dynamic Link/"+("Contact Type")+":100"] + [("Contact Type") + ":Data:100"] + [("Start Time") + ":Data:100"] + \
 			  [("End Time") + ":Data:80"] + [("Call Length") + ":Data:80"] + \
-			  [("Agent/Call attendant") + ":Data:180"] + [("Call Log Id") + ":Link/Call Logs:"]
+			  [("Agent/Call attendant") + ":Link/User:180"] + [("Subject") + ":Small Text:180"]
 	return columns
