fd6233a42c52591d764f285b9ac5cec2dec9c3c7
khatrijitendra <jitendra.k@indictranstech.com>
2016-11-09 12:21:49 +0530

    [Feature]make call from call logs

--- switsol/switsol/doctype/call_logs/call_logs.js
+++ switsol/switsol/doctype/call_logs/call_logs.js
@@ -3,6 +3,9 @@
 
 frappe.ui.form.on('Call Logs', {
 	refresh: function(frm) {
-
+		if(!cur_frm.doc.__islocal && cur_frm.doc.phone_number){			
+			html = "<a href='vdsip:" + cur_frm.doc.phone_number + "'><i class='icon-phone' aria-hidden='true'></i> <b>Call</b></a>"
+			$(cur_frm.fields_dict.outgoing_call.wrapper).html(html);
+		}
 	}
 });
