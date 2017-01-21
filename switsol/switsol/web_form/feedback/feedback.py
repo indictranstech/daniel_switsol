ca51b97f6aaab5ae1ba9533699f95c4811282e00
khatrijitendra <jitendra.k@indictranstech.com>
2017-01-21 10:59:01 +0530

    [Fix]filter feedback project

--- switsol/switsol/web_form/feedback/feedback.py
+++ switsol/switsol/web_form/feedback/feedback.py
@@ -5,3 +5,16 @@ import frappe
 def get_context(context):
 	# do your magic here
 	pass
+
+
+@frappe.whitelist(allow_guest=True)
+def get_list(doctype, fields=None, filters=None, order_by=None,
+	limit_start=None, limit_page_length=20):
+	return frappe.get_list(doctype, fields=fields, filters=filters, order_by=order_by,
+		limit_start=limit_start, limit_page_length=limit_page_length, ignore_permissions=True)
+
+# @frappe.whitelist(allow_guest=True)
+# def get_data(cond):
+# 	print "####################",cond
+# 	data = frappe.db.sql("""select name from `tabProject` where status = "Open" and name = "{0}" """.format(cond))
+# 	return data
