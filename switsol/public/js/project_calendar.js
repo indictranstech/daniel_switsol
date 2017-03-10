36adf5c6940ec983a632b33b8c9438e934302fcf
khatrijitendra <jitendra.k@indictranstech.com>
2017-03-10 12:40:36 +0530

    [Feature]Added Calendar for Project

---
+++ switsol/public/js/project_calendar.js
@@ -0,0 +1,12 @@
+
+frappe.views.calendar["Project"] = {
+	field_map: {
+		"start": "start_date",
+		"end": "end_date",
+		"id": "name",
+		"title": "name",
+		"Room" : "room"
+		//"allDay": "allDay"
+	},
+	get_events_method: "switsol.custom_script.project.get_events"
+}
