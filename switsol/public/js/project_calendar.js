ef7321532528bccd363906d2e7cdcf80ab11f600
chaitraliw <chaitrali.w@indictranstech.com>
2017-08-12 17:20:52 +0530

    [Feature]Added Walkin Rooms to Calendar

--- switsol/public/js/project_calendar.js
+++ switsol/public/js/project_calendar.js
@@ -6,17 +6,6 @@ frappe.views.calendar["Project"] = {
 		"id": "name",
 		"title": "name",
 		"Room" : "room"
-		//"allDay": "allDay"
 	},
-	filters: [
-	{
-		"fieldtype": "Button",
-		"fieldname": "course_schedule",
-		"label": __("Course Schedule"),
-		"click": function(){
-			frappe.set_route("Calendar","Course Schedule")
-		}
-	}
-],
 	get_events_method: "switsol.custom_script_project.project.get_events"
 }
